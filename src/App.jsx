8303ff5b9e850e36786db1f4ada023dd35eee571
Andysnabb <anders@skaalgaard.com>
2026-06-11 00:44:15 +0200

    Refactor App component for navigation links
    Refactor navigation links to use NavLinkButton component for active link styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Link, Routes, Route } from "react-router-dom";
+import { Link, Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Part1Page from "./pages/Part1Page";
 import Part2Page from "./pages/Part2Page";
@@ -7,30 +7,46 @@ import AnswersPage from "./pages/AnswersPage";
 import LeaderboardPage from "./pages/LeaderboardPage";
 import LivePage from "./pages/LivePage";
 
+function NavLinkButton({ to, children }) {
+    const location = useLocation();
+    const isActive = location.pathname === to;
+
+    return (
+        <Link
+            to={to}
+            className={`nav-button ${isActive ? "active" : ""}`}
+        >
+            {children}
+        </Link>
+    );
+}
+
 export default function App() {
     return (
-        <div style={{ maxWidth: 960, margin: "0 auto", padding: 20 }}>
+        <div className="container">
             <h1>VM Tipping 2026</h1>
 
-            <nav style={{ marginBottom: 20, display: "flex", gap: 10, flexWrap: "wrap" }}>
-                <Link to="/">Hjem</Link>
-                <Link to="/part1">Del 1</Link>
-                <Link to="/part2">Del 2</Link>
-                <Link to="/part3">Del 3</Link>
-                <Link to="/answers">Alle svar</Link>
-                <Link to="/leaderboard">Poeng</Link>
-                <Link to="/live">Livedata</Link>
+            <nav className="nav-buttons">
+                <NavLinkButton to="/">Hjem</NavLinkButton>
+                <NavLinkButton to="/part1">Del 1</NavLinkButton>
+                <NavLinkButton to="/part2">Del 2</NavLinkButton>
+                <NavLinkButton to="/part3">Del 3</NavLinkButton>
+                <NavLinkButton to="/answers">Alle svar</NavLinkButton>
+                <NavLinkButton to="/leaderboard">Poeng</NavLinkButton>
+                <NavLinkButton to="/live">Livedata</NavLinkButton>
             </nav>
 
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/part1" element={<Part1Page />} />
-                <Route path="/part2" element={<Part2Page />} />
-                <Route path="/part3" element={<Part3Page />} />
-                <Route path="/answers" element={<AnswersPage />} />
-                <Route path="/leaderboard" element={<LeaderboardPage />} />
-                <Route path="/live" element={<LivePage />} />
-            </Routes>
+            <div className="card">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/part1" element={<Part1Page />} />
+                    <Route path="/part2" element={<Part2Page />} />
+                    <Route path="/part3" element={<Part3Page />} />
+                    <Route path="/answers" element={<AnswersPage />} />
+                    <Route path="/leaderboard" element={<LeaderboardPage />} />
+                    <Route path="/live" element={<LivePage />} />
+                </Routes>
+            </div>
         </div>
     );
 }
