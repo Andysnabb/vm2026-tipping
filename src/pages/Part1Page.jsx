261d99b7fe0745dca4107840ae14a7a1dc28c9b4
Andysnabb <anders@skaalgaard.com>
2026-06-12 09:34:49 +0200

    Update Part1Page.jsx

--- src/pages/Part1Page.jsx
+++ src/pages/Part1Page.jsx
@@ -39,7 +39,7 @@ export default function Part1Page() {
     const [loading, setLoading] = useState(false);
 
     // SPERREDATO SETT TIL 11. JUNI 2026 KL. 21:05
-    const SPERRE_DATO = new Date("2026-06-11T21:05:00");
+    const SPERRE_DATO = new Date("2026-06-12T21:05:00");
     const nå = new Date();
     const erLåst = nå >= SPERRE_DATO; // MERK: Vi bruker >= siden siden skal LÅSES fra og med dette tidspunktet
 
