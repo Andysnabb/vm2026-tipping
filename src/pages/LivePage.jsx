e6d5e0d18cbf4c06578830da97365b81c876457b
Andysnabb <anders@skaalgaard.com>
2026-06-15 16:19:45 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- src/pages/LivePage.jsx
+++ src/pages/LivePage.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useMemo, useState } from "react";
 
 const STANDINGS_URL =
-    "https://script.google.com/macros/s/AKfycbyaNflTNpa89Z-iHyqaQobzcRUPSMcA6G5K-FWZCJuHYdtsDpf_PdisN_ku2596jafe/exec";
+    "https://script.google.com/macros/s/AKfycbzrQOHVcdiBMVpJhxscs3UbmA1M5GJkShf9jBOpNUKB0oJzse3IBoa2qsoBM0EMNBgl/exec";
 const BRACKET_URL =
-    "https://script.google.com/macros/s/AKfycbyaNflTNpa89Z-iHyqaQobzcRUPSMcA6G5K-FWZCJuHYdtsDpf_PdisN_ku2596jafe/exec";
+    "https://script.google.com/macros/s/AKfycbzrQOHVcdiBMVpJhxscs3UbmA1M5GJkShf9jBOpNUKB0oJzse3IBoa2qsoBM0EMNBgl/exec";
 
 function pick(...values) {
     for (const v of values) {
